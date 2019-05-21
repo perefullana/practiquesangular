483ee7c72fa3b1e4070bc46e65c078a47207240b
Pere Fullana <Pere.Fullana@vanderlande.com>
2019-05-21 12:07:00 +0200

    Inicializamos el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'HelloWorld';
+
+export class AppComponent implements OnInit {
+  constructor() {}
+  title = 'Joan Joanet';
+  public field1: number;
+  public field2: number;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
