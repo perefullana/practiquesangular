f5519d8074bb3d2e0175b1ca199504896cb7338d
Pere Fullana <Pere.Fullana@vanderlande.com>
2019-05-21 12:36:14 +0200

    Trabajo en la rama de desarollo de la calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Calculator } from './Calculator';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-  constructor() {}
-  title = 'Joan Joanet';
   public field1: number;
   public field2: number;
+  public result: any;
+  public calc: Calculator;
+  constructor() {
+    this.calc = new Calculator();
+  }
+  sum() {
+  this.result = this.calc.sum(this.field1, this.field2);
+  }
+  substract() {
+  this.result = this.calc.substract(this.field1, this.field2);
+  }
+  multiply() {
+  this.result = this.calc.multiply(this.field1, this.field2);
+  }
+  divide(){
+  this.result = this.calc.divide(this.field1, this.field2);
+  }
+  insultarjulio(){
+    this.result = this.calc.insultarjulio('hola');
+  }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
 }
